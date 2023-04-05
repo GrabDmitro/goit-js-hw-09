e460cec988780de3785f3509cee1ea136e813ab5
GrabDmitro <d_grab2001@ukr.net>
2023-04-06 00:54:27 +0300

    start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+var intervalId = null;
+refs.stop.disabled = true;
 
+refs.start.addEventListener('click', e => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  intervalId = setInterval(
+    () => (refs.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+});
+
+refs.stop.addEventListener('click', e => {
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+  clearInterval(intervalId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
